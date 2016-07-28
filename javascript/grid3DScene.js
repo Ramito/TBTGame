d107fccbccf4d94e7c09fad1be9fdf8025c387cc
Ramito <elramito@gmail.com>
2016-07-27 22:36:08 -0700

    Refactored grid3DScene
    It requires less redundant arguments

--- javascript/grid3DScene.js
+++ javascript/grid3DScene.js
@@ -1,42 +1,61 @@
 var THREE = require('three');
 
 var Grid3DScene = {
-    renderGrid: function(grid, cubeSize, heightFactor, scene) { //TODO: Cache values touse in position dispatch?
-        var cubeGeometry = new THREE.CubeGeometry(cubeSize, heightFactor, cubeSize);
-        var cubeMaterial = new THREE.MeshLambertMaterial({
-            color: 0x1ec876
-        });
+    create: function(grid, gridSize, heightFactor) {
+        var scene = {};
 
-        var heightPropertyName = "height";
-        var gridHasHeight = grid.hasGridProperty(heightPropertyName);
+        scene.render = function(renderScene) {
+            var cubeGeometry = new THREE.CubeGeometry(gridSize, heightFactor, gridSize);
+            var cubeMaterial = new THREE.MeshLambertMaterial({
+                color: 0x1ec876
+            });
 
-        var max = grid.columns * grid.rows;
-        for (var cell = 0; cell < max; ++cell) {
-            var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-            cube.position.x = grid.getCellX(cell) * cubeSize;
-            cube.position.y = 0;
-            cube.position.z = grid.getCellY(cell) * cubeSize;
-            scene.add(cube);
-            if (gridHasHeight) {
-                for (var height = 1; height <= grid.getGridPropertyValue(heightPropertyName, cell); ++height) {
-                    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-                    cube.position.x = grid.getCellX(cell) * cubeSize;
-                    cube.position.y = height * heightFactor;
-                    cube.position.z = grid.getCellY(cell) * cubeSize;
-                    scene.add(cube);
+            var heightPropertyName = "height";
+            var gridHasHeight = grid.hasGridProperty(heightPropertyName);
+
+            var max = grid.columns * grid.rows;
+            for (var cell = 0; cell < max; ++cell) {
+                var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+                cube.position.x = grid.getCellX(cell) * gridSize;
+                cube.position.y = 0;
+                cube.position.z = grid.getCellY(cell) * gridSize;
+                renderScene.add(cube);
+                if (gridHasHeight) {
+                    for (var height = 1; height <= grid.getGridPropertyValue(heightPropertyName, cell); ++height) {
+                        cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+                        cube.position.x = grid.getCellX(cell) * gridSize;
+                        cube.position.y = height * heightFactor;
+                        cube.position.z = grid.getCellY(cell) * gridSize;
+                        renderScene.add(cube);
+                    }
                 }
             }
-        }
-    },
+        };
 
-    setGridScenePosition: function(cell, inGrid, positionToSet, squareSize, squareHeight, additionalHeightOffset) {
-        positionToSet.x = inGrid.getCellX(cell) * squareSize;
-        positionToSet.y = squareHeight * 0.5 + additionalHeightOffset;
-        positionToSet.z = inGrid.getCellY(cell) * squareSize;
-        if (inGrid.hasGridProperty('height')) {
-            positionToSet.y += squareHeight * inGrid.getGridPropertyValue('height', cell);
-        }
-    }
+        scene.getPositionInScene = function(cell) {
+            var position = {
+                x: 0,
+                y: 0,
+                z: 0
+            };
+            position.x = grid.getCellX(cell) * gridSize;
+            position.y = heightFactor * 0.5;
+            position.z = grid.getCellY(cell) * gridSize;
+            if (grid.hasGridProperty('height')) {
+                position.y += heightFactor * grid.getGridPropertyValue('height', cell);
+            }
+            return position;
+        };
+
+        scene.setPositionInScene = function(cell, positionToSet, heightOffset) {
+            var scenePosition = scene.getPositionInScene(cell);
+            positionToSet.x = scenePosition.x;
+            positionToSet.y = scenePosition.y + heightOffset;
+            positionToSet.z = scenePosition.z;
+        };
+
+        return scene;
+    },
 };
 
 module.exports = Grid3DScene;
